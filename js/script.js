b1bbed59f88432747ad9a2ab2bd7fe726b9f2058
Gustavo Lopes <gustavoclash3@gmail.com>
2023-01-29 21:00:52 -0300

    end project

--- js/script.js
+++ js/script.js
@@ -1,11 +1,34 @@
 
 const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
 
 const jump = () => {
     mario.classList.add('jump');
     setTimeout(() => {
-mario.classList.remove('jump')
+        mario.classList.remove('jump')
     }, 500)
 }
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+    
+
+    if (pipePosition <= 89 && pipePosition > 0 && marioPosition <= 105) {
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosition}px`;
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`;
+        mario.src = '/images/bolsonaro.jpg'
+        mario.style.width = '300px'
+    
+        clearInterval(loop)
+
+
+    }
+
+}, 10)
+
 
 document.addEventListener('keydown', jump)
